62f9e5e69527ac2e7c16826c0d8db080d2099bd7
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-10 18:13:53 +0700

    menunjuk ke sebuah nama file

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -9,5 +9,8 @@ int main() {
     cout << "Masukan Nama File : ";
     cin >> NamaFile;
     //membuka file dalam mode menulis.
-   
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(NamaFile + ".txt", ios::out);
+    cout << ">= Menulis file \'q\' untuk keluar" << endl;
 }
