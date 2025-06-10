368cc1aec518d29faa4cdcf367aef5552b0e0edb
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-10 17:39:16 +0700

    menutup infile.close

--- ioFile.cpp
+++ ioFile.cpp
@@ -31,7 +31,15 @@ int main() {
     //jika file ada maka
     if (infile.is_open())
     {
-        
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            //dan tampilkan disini
+            cout << baris << '\n';
+
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
     }
 
 }
