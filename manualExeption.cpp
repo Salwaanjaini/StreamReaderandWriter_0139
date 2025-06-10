d3f3eba5ff24d142b554b448f7cffe97af9948da
Salwaanjaini27 <salwaanjainifutriendsani@gmail.com>
2025-06-10 18:46:07 +0700

    melemparkan sebuah integer maka

---
+++ manualExeption.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di prodi TI UMY" << endl;
+        throw 0.5; // melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan di eksekusi" << endl;
+
+    }
+   
+}
